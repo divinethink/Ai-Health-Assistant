75fba16220808c1527ba949ef4ea8497dff90369
Hasan <cheletiboka@gmail.com>
2026-09-03 11:44:01 +0600

    Add files via upload

--- src/globals.js
+++ src/globals.js
@@ -21,7 +21,6 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
 import "firebase/compat/app-check";
-import "firebase/compat/storage";
 
 window.React = React;
 window.ReactDOM = ReactDOM;
